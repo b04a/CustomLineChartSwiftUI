f89e29dd6504d461eddded3524369a727c959751
+futurehendrix <107930591+plusfuturehendrix@users.noreply.github.com>
2022-12-24 19:01:06 +0300

    Add files via upload

---
+++ chartTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chartTest
+//
+//  Created by Danil Bochkarev on 24.12.2022.
+//
+
+import SwiftUI
+import Charts
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chartTest/chartTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  chartTestApp.swift
+//  chartTest
+//
+//  Created by Danil Bochkarev on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct chartTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
